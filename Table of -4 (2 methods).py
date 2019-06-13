ec78143009074eb452d138191276292582ea08c9
SYED RABEET UR REHMAN <rabeetsyed2@gmail.com>
2019-06-13 16:12:18 +0500

    Table of -4 using loop,range .
    1st method : range is -ve and number is +ve
    
    2nd method : range is +ve and number is -ve

---
+++ Table of -4 (2 methods).py
@@ -0,0 +1,13 @@
+print('TABEL OF -ve 4')
+
+for A in range(0,-100,-1) :
+    print('4 x ' + str(A) + " = " + str( A * 4))
+
+
+
+
+print('TABEL OF -ve 4')
+
+for A in range(100) :
+    print('-4 x ' + str(A) + " = " + str( A * -4))
+
